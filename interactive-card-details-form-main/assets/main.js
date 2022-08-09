8d9cc0af7dbc4d4695bdb218c46c58fbd9f50edc
afifhibatullahh <afifhibatullah59@gmail.com>
2022-08-09 11:07:35 +0700

    update solustion

--- interactive-card-details-form-main/assets/main.js
+++ interactive-card-details-form-main/assets/main.js
@@ -1,7 +1,7 @@
 const form = document.getElementById("form");
 const username = document.getElementById("username");
 const number = document.getElementById("number");
-const month = document.getElementById("form");
+const month = document.getElementById("month");
 const year = document.getElementById("year");
 const cvc = document.getElementById("cvc");
 
@@ -11,24 +11,105 @@ form.addEventListener("submit", (e) => {
   checkInputs();
 });
 
+number.addEventListener("input", function (e) {
+  e.target.value = e.target.value
+    .replace(/[^\dA-Z]/g, "")
+    .replace(/(.{4})/g, "$1 ")
+    .trim();
+
+  const cardNumber = document.getElementsByClassName("card-number");
+  cardNumber[0].innerText = e.target.value;
+});
+
+username.addEventListener("input", function (e) {
+  const cardholder = document.getElementsByClassName("cardholder-name");
+  cardholder[0].innerText = e.target.value;
+});
+
+month.addEventListener("input", function (e) {
+  const month = document.getElementsByClassName("month");
+  month[0].innerText = e.target.value;
+});
+year.addEventListener("input", function (e) {
+  const year = document.getElementsByClassName("year");
+  year[0].innerText = e.target.value;
+});
+
 function checkInputs() {
+  let isValidate = 0;
   const usernameValue = username.value.trim();
   const numberValue = number.value.trim();
   const monthValue = month.value.trim();
   const yearValue = year.value.trim();
   const cvcValue = cvc.value.trim();
 
+  const regNum = /^[0-9]+$||\s*/;
+
   if (usernameValue === "") {
     setErrorFor(username, "Name can't be blank");
   } else {
     setSuccessFor(username);
+    isValidate++;
+  }
+
+  if (regNum.test(numberValue)) {
+    if (numberValue.length == 19) {
+      setSuccessFor(number);
+      isValidate++;
+    } else setErrorFor(number, "Should contain 16 numbers");
+  } else setErrorFor(number, "Wrong format, numbers only");
+
+  if (monthValue === "") setErrorFor(month, "Can't be blank", true);
+  else {
+    setSuccessFor(month, true);
+    isValidate++;
+  }
+
+  if (yearValue === "") setErrorFor(year, "Can't be blank", true);
+  else {
+    isValidate++;
+    setSuccessFor(year, true);
+  }
+
+  if (cvcValue == "") {
+    setErrorFor(cvc, "Can't be blank");
+  } else {
+    if (cvcValue.length >= 3) {
+      setSuccessFor(cvc);
+      isValidate++;
+    } else setErrorFor(cvc, "Should contain at least 3 characters");
+  }
+
+  if (isValidate == 5) {
+    // hide form
+    form.classList.toggle("hide");
+    // set text button
+    const createdCard = document.getElementById("created-success");
+    createdCard.classList.toggle("hide");
   }
 }
 
-function setErrorFor(input, message) {
-  const formControl = input.parentElement;
+function setErrorFor(input, message, isDate = false) {
+  let formControl = input.parentElement;
+  formControl.className = "form-control error";
+  if (isDate) {
+    formControl = formControl.parentElement.parentElement.parentElement;
+    formControl.className = "form-control error";
+  }
+
   const small = formControl.querySelector("small");
 
   small.innerText = message;
-  formControl.className = "form-control error";
+  small.style.visibility = "visible";
+}
+
+function setSuccessFor(input, isDate = false) {
+  let formControl = input.parentElement;
+  formControl.className = "form-control success";
+  if (isDate) {
+    formControl = formControl.parentElement.parentElement.parentElement;
+    formControl.className = "form-control success";
+  }
+  const small = formControl.querySelector("small");
+  small.style.visibility = "hidden";
 }
