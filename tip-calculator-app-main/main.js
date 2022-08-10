e8744efdc32242348b1c390b8e6bec5184628fdc
afifhibatullahh <afifhibatullah59@gmail.com>
2022-08-10 16:49:25 +0700

    update js for splitter

--- tip-calculator-app-main/main.js
+++ tip-calculator-app-main/main.js
@@ -1,1 +1,47 @@
-let tip5 = document.getElementsByClassName()
+const tip5 = document.getElementById("tip-5");
+const tip10 = document.getElementById("tip-10");
+const tip15 = document.getElementById("tip-15");
+const tip25 = document.getElementById("tip-25");
+const tip50 = document.getElementById("tip-50");
+const tipCustom = document.getElementById("tip-custom");
+const bill = document.getElementById("bill");
+const numPeople = document.getElementById("num-people");
+
+const box = [tip5, tip10, tip15, tip25, tip50, tipCustom];
+let picked;
+let tip = 0;
+
+box.forEach((element) => {
+  element.addEventListener("click", (e) => {
+    tip = parseInt(e.target.textContent);
+    if (picked != undefined) picked.classList.remove("pick");
+    picked = e.target;
+    e.target.classList.add("pick");
+    totalbill(tip, bill.value, numPeople.value);
+  });
+});
+
+bill.addEventListener("input", (e) => {
+  totalbill(tip, parseInt(e.target.value), parseInt(numPeople.value));
+});
+
+numPeople.addEventListener("input", (e) => {
+  totalbill(tip, parseInt(bill.value), parseInt(e.target.value));
+});
+
+const btnReset = document.getElementById("btn-reset");
+
+btnReset.addEventListener("click", () => {
+  totalbill(0, 0, 0);
+});
+
+function totalbill(tip = 0, bill = 0, people = 0) {
+  let tips = 0;
+  if (people >= 1) {
+    tips = bill * parseFloat(tip / 100);
+  }
+  let total = parseFloat(bill / people);
+
+  document.getElementById("tip-person").innerText = parseFloat(total);
+  document.getElementById("tip-number").innerText = parseFloat(tips);
+}
